77c46b52b1399069c70e58dc31839f3dea54f7d0
Murilo-MRS <murilosantana1993@gmail.com>
2023-03-22 17:19:40 -0300

    feat: add middleware de erro

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,10 @@
 import express from 'express';
+// import HandleGlobalError from './Middlewares/HandleGlobalError';
+// import routes from './Routes';
 
 const app = express();
+app.use(express.json());
+// app.use(routes);
+// app.use(HandleGlobalError.handleError);
 
 export default app;
